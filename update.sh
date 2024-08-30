d423ff47f273d2e0595a985071382a45441ba6bc
om3ga <om3ga6400gd@gmail.com>
2024-08-30 21:56:41 +0000

    updater worky

--- update.sh
+++ update.sh
@@ -10,19 +10,19 @@ mkdir cookies/loc
 
 # Download files for 'lib'
 cd lib
-wget -N -i ../liblist.txt -B https://coderpatsy.bitbucket.io/lib/
+wget -N -i ../_liblist.txt -B https://coderpatsy.bitbucket.io/lib/
 
 # Download files for 'cookies'
 cd ../cookies
-wget -N -i ../cookieslist.txt -B https://coderpatsy.bitbucket.io/cookies/
+wget -N -i ../_cookieslist.txt -B https://coderpatsy.bitbucket.io/cookies/
 
 # Download image files for 'cookies'
 cd img
-wget -N -i ../../imglist.txt -B https://coderpatsy.bitbucket.io/cookies/img/
+wget -N -i ../../_imglist.txt -B https://coderpatsy.bitbucket.io/cookies/img/
 
 # Download location files for 'cookies'
 cd ../loc
-wget -N -i ../../loclist.txt -B https://coderpatsy.bitbucket.io/cookies/loc/
+wget -N -i ../../_loclist.txt -B https://coderpatsy.bitbucket.io/cookies/loc/
 
 # Download the editor HTML file
 cd ../
