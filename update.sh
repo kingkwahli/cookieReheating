c9ae900c364163171686375daafd104e5422098f
om3ga <om3ga6400gd@gmail.com>
2024-08-31 18:17:17 +0000

    yay i fixed it

--- update.sh
+++ update.sh
@@ -3,6 +3,13 @@ rm -r img
 rm -r lib
 rm -r loc
 
+# Remove existing files
+rm -f editor_minigames.js
+rm -f editor_start.js
+rm -f editor.css
+rm -f editor.js
+rm -f savelib.js
+
 # Create new directories
 mkdir img
 mkdir lib
@@ -22,7 +29,7 @@ wget -N -i ../update/_loclist.txt -B https://coderpatsy.bitbucket.io/cookies/loc
 
 # Download files for 'root'
 cd ../
-wget -N -i ../update/_rootlist.txt -B https://coderpatsy.bitbucket.io/cookies/
+wget -N -i update/_rootlist.txt -B https://coderpatsy.bitbucket.io/cookies/
 
 # Download the editor HTML file
 wget -O index.html https://coderpatsy.bitbucket.io/cookies/editor.html
